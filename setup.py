d33a06a2e6607ee11f16ac4550fc9926d8b9b5e3
JVM <vaghasiyajeel2006@gmail.com>
2026-07-12 14:22:54 +0530

    chore: add requirements.txt and setup.py configuration

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="youtube_sentiment_analysis",
+    version="0.1.0",
+    author="Jeel Vaghasiya",
+    description="An MLOps pipeline using a Stacking Classifier for YouTube comment sentiment insights.",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
